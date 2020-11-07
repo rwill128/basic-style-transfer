4949dc053022677fa6cdbdb6158d4c3bc8870e6e
Richard Williams <rwill128.create@gmail.com>
2020-11-07 00:26:21 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,75 @@
+# %%
+
+import functools
+import os
+
+from matplotlib import gridspec
+import matplotlib.pylab as plt
+import numpy as np
+import tensorflow as tf
+import tensorflow_hub as hub
+
+print("TF Version: ", tf.__version__)
+print("TF-Hub version: ", hub.__version__)
+print("Eager mode enabled: ", tf.executing_eagerly())
+print("GPU available: ", tf.test.is_gpu_available())
+
+
+# %%
+
+# @title Define image loading and visualization functions  { display-mode: "form" }
+
+def crop_center(image):
+    """Returns a cropped square image."""
+    shape = image.shape
+    new_shape = min(shape[1], shape[2])
+    offset_y = max(shape[1] - shape[2], 0) // 2
+    offset_x = max(shape[2] - shape[1], 0) // 2
+    image = tf.image.crop_to_bounding_box(
+        image, offset_y, offset_x, new_shape, new_shape)
+    return image
+
+
+@functools.lru_cache(maxsize=None)
+def load_image(image_url, image_size=(256, 256), preserve_aspect_ratio=True):
+    """Loads and preprocesses images."""
+    # Cache image file locally.
+    image_path = tf.keras.utils.get_file(os.path.basename(image_url)[-128:], image_url)
+    # Load and convert to float32 numpy array, add batch dimension, and normalize to range [0, 1].
+    img = plt.imread(image_path).astype(np.float32)[np.newaxis, ...]
+    if img.max() > 1.0:
+        img = img / 255.
+    if len(img.shape) == 3:
+        img = tf.stack([img, img, img], axis=-1)
+    img = crop_center(img)
+    img = tf.image.resize(img, image_size, preserve_aspect_ratio=True)
+    return img
+
+
+def show_n(images, titles=('',)):
+    n = len(images)
+    for i in range(n):
+        print(images[i][0].shape)
+        plt.imsave('C:/Users/Rick/Pictures/style-transfer.png', images[i][0], format='png')
+
+
+content_image_url = 'file:/C:/Users/Rick/Pictures/portrait.jpg'  # @param {type:"string"}
+style_image_url = 'file:/C:/Users/Rick/Pictures/Sketch001.jpg'  # @param {type:"string"}
+
+output_image_size = 3000  # @param {type:"integer"}
+style_size = 512
+
+content_img_size = (output_image_size, output_image_size)
+style_img_size = (style_size, style_size)  # Recommended to keep it at 256.
+
+content_image = load_image(content_image_url, content_img_size)
+style_image = load_image(style_image_url, style_img_size)
+style_image = tf.nn.avg_pool(style_image, ksize=[3, 3], strides=[1, 1], padding='SAME')
+
+hub_handle = 'https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/2'
+hub_module = hub.load(hub_handle)
+
+outputs = hub_module(tf.constant(content_image), tf.constant(style_image))
+stylized_image = outputs[0]
+
+show_n([stylized_image], titles=['Stylized image'])
